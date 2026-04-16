9873f761390b8527e1dea5ec2cd8d101dc543b8d
Augustin Delale <adelale@hotmail.com>
2026-04-16 08:43:13 +0200

    mise à jour de gus.y.entitydb1

---
+++ src/a/entity/gus/y/entitydb1/entity_import/count/byimport/EntityImpl.java
@@ -0,0 +1,42 @@
+package a.entity.gus.y.entitydb1.entity_import.count.byimport;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import a.framework.*;
+
+public class EntityImpl implements Entity, T {
+
+	public String creationDate() {return "20260416";}
+
+	public static final String TABLE_NAME = "entity_import";
+
+	public static final String COL_ENTITY_NAME = "entity_name";
+	public static final String COL_ENTITY_IMPORT = "entity_import";
+
+	public Object t(Object obj) throws Exception
+	{
+		Connection cx = (Connection) obj;
+		String sql = "SELECT " + COL_ENTITY_IMPORT + ", COUNT(*) FROM " + 
+		TABLE_NAME + " GROUP BY " + COL_ENTITY_IMPORT;
+
+		try (
+			PreparedStatement st = cx.prepareStatement(sql);
+			ResultSet rs = st.executeQuery())
+		{
+			Map data = new HashMap();
+			while (rs.next())
+			{
+				String import_ = rs.getString(1);
+				Integer count = (Integer) rs.getInt(2);
+				data.put(import_, count);
+			}
+			return data;
+		}
+	}
+}
---
+++ src/a/entity/gus/y/entitydb1/entity_import/find2/in/EntityImpl.java
@@ -0,0 +1,66 @@
+package a.entity.gus.y.entitydb1.entity_import.find2.in;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.HashSet;
+import java.util.Set;
+import a.framework.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class EntityImpl implements Entity, T {
+
+	public String creationDate() {return "20260416";}
+
+	public static final String TABLE_NAME = "entity_import";
+
+	public static final String COL_ENTITY_NAME = "entity_name";
+	public static final String COL_ENTITY_IMPORT = "entity_import";
+
+	public Object t(Object obj) throws Exception
+	{
+		Object[] o = (Object[]) obj;
+		if (o.length != 2) throw new Exception("Wrong data number: " + o.length);
+
+		Connection cx = (Connection) o[0];
+		Set imports = (Set) o[1];
+		
+		if(imports==null) return new HashSet();
+		if(imports.isEmpty()) return new HashSet();
+
+		StringBuffer b = new StringBuffer();
+		b.append("SELECT ");
+		b.append(COL_ENTITY_NAME);
+		b.append(" FROM ");
+		b.append(TABLE_NAME);
+		b.append(" WHERE ");
+		b.append(COL_ENTITY_IMPORT);
+		b.append(" IN (");
+
+		int nb = imports.size();
+		for (int i = 0; i < nb; i++)
+		{
+			b.append("?");
+			if (i < nb - 1) b.append(",");
+		}
+		b.append(")");
+		
+		String sql = b.toString();
+		List importList = new ArrayList(imports);
+		
+		PreparedStatement st = cx.prepareStatement(sql);
+		for (int i = 0; i < nb; i++) 
+		st.setObject(i + 1, importList.get(i));
+		ResultSet rs = st.executeQuery();
+
+		Set data = new HashSet();
+		while (rs.next())
+		{
+			String entityName = (String) rs.getObject(COL_ENTITY_NAME);
+			data.add(entityName);
+		}
+		st.close();
+		return data;
+	}
+}
