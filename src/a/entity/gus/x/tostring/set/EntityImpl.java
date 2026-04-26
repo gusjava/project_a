0ad953371ef77b1d1f2af4d8553b85ea46744921
Augustin Delale <adelale@hotmail.com>
2026-04-26 15:17:33 +0200

    Suppression de gus06.x.tostring.set

--- src/a/entity/gus/x/tostring/set/EntityImpl.java
+++ src/a/entity/gus/x/tostring/set/EntityImpl.java
@@ -2,28 +2,26 @@ package a.entity.gus.x.tostring.set;
 
 import java.util.Iterator;
 import java.util.Set;
-
-import a.framework.Entity;
-import a.framework.T;
+import a.framework.*;
 
 public class EntityImpl implements Entity, T {
 	public String creationDate() {return "20231128";}
 
-	public Object t(Object obj) throws Exception {
-		return setToString((Set) obj);
-	}
+	public Object t(Object obj) throws Exception
+	{return setToString((Set) obj);}
 
-	private String setToString(Set set) throws Exception {
+	private String setToString(Set set) throws Exception
+	{
 		StringBuffer b = new StringBuffer();
 		Iterator it = set.iterator();
-		while (it.hasNext()) {
+		while (it.hasNext())
+		{
 			String el = (String) it.next();
 			if (el.contains("\n"))
 				throw new Exception("Invalid element: " + el);
 			b.append(el + "\n");
 		}
-		if (b.length() > 0)
-			b.deleteCharAt(b.length() - 1);
+		if (b.length() > 0) b.deleteCharAt(b.length() - 1);
 		return b.toString();
 	}
 }
